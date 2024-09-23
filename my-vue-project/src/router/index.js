1f655dc96bc4f74680c55b095a04cc20ef146ea0
hmmmq <1053825066@qq.com>
2024-09-23 19:02:01 +0800

    update

--- my-vue-project/src/router/index.js
+++ my-vue-project/src/router/index.js
@@ -1,9 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
-
+import IndexPage from "@/views/IndexPage.vue";
 // 引入PG组件
 
 // 路由配置
-const routes = [{ path: "/:pathMatch(.*)*", component: <div>404</div> }];
+const routes = [
+  { path: "/", component: IndexPage },
+  { path: "/:pathMatch(.*)*", component: <div>404</div> },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
