e3d826b1a1858e9db5d5bcd5a0a06bf7063bf2d9
hmmmq <1053825066@qq.com>
2024-09-23 16:37:34 +0800

    update

--- my-vue-project/src/App.vue
+++ my-vue-project/src/App.vue
@@ -3,9 +3,4 @@
     <router-view></router-view>
   </div>
 </template>
-  <style scoped>
-  /* 引入的样式文件 */
-  @import '@/assets/css/bootstrap.min.css';
-  @import '@/assets/css/style.css';
-  </style>
-  
+<style scoped></style>
